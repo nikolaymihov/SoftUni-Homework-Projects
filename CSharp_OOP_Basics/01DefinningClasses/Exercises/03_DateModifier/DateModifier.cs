077e07ebde8a09adbe89f4258c353d6a99b3ccb1
Nikolay Mihov <Nikolay.Mihov96@gmail.com>
2020-04-05 22:35:14 +0300

    Adding DateModifier task

---
+++ CSharp_OOP_Basics/01DefinningClasses/Exercises/03_DateModifier/DateModifier.cs
@@ -0,0 +1,53 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class DateModifier
+    {
+        private string firstDate;
+        private string secondDate;
+
+        public DateModifier(string firstDate, string secondDate)
+        {
+            this.FirstDate = firstDate;
+            this.SecondDate = secondDate;
+        }
+
+        public string FirstDate
+        {
+            get
+            {
+                return this.firstDate;
+            }
+            set
+            {
+                this.firstDate = value;
+            }
+                
+        }
+
+        public string SecondDate
+        {
+            get
+            {
+                return this.secondDate;
+            }
+            set
+            {
+                this.secondDate = value;
+            }
+        }
+
+        public double getDifferenceInDays (string firstDate, string secondDate)
+        {
+            DateTime dateOne = Convert.ToDateTime(firstDate);
+            DateTime dateTwo = Convert.ToDateTime(secondDate);
+
+            double diffInDays = (dateOne - dateTwo).TotalDays;
+
+            return diffInDays;
+        }
+    }
+}
---
+++ CSharp_OOP_Basics/01DefinningClasses/Exercises/03_DateModifier/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace DefiningClasses
+{
+    using DefiningClasses;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            DateModifier modifier = new DateModifier(firstDate, secondDate);
+            double daysBetween = modifier.getDifferenceInDays(modifier.FirstDate, modifier.SecondDate);
+            
+            Console.WriteLine(Math.Abs(daysBetween));
+        }
+    }
+}
