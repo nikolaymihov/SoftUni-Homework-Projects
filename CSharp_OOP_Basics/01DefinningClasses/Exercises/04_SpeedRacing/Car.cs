934a8a57bbf4c215bb5ec7162280e098fc86f78b
Nikolay Mihov <Nikolay.Mihov96@gmail.com>
2020-04-06 00:48:56 +0300

    Adding SpeedRacing task

---
+++ CSharp_OOP_Basics/01DefinningClasses/Exercises/04_SpeedRacing/Car.cs
@@ -0,0 +1,97 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKilometer;
+        private double travelledDistance;
+
+        public Car() : this(null, 0, 1, 0)
+        {
+
+        }
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer) 
+            : this(model, fuelAmount, fuelConsumptionPerKilometer, 0)
+        {
+
+        }
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer, double travelledDistance)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            this.TravelledDistance = travelledDistance;
+        }
+        
+        public string Model { get; private set; }
+
+        public double FuelAmount
+        {
+            get {return this.fuelAmount; }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new InvalidOperationException("The amount of fuel cannot be a negative number.");
+                }
+                else
+                {
+                    this.fuelAmount = value;
+                }
+            }
+        }
+
+        public double FuelConsumptionPerKilometer
+        {
+            get { return this.fuelConsumptionPerKilometer; }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new InvalidOperationException("The fuel consumption should be higher than 0.");
+                }
+                else
+                {
+                    this.fuelConsumptionPerKilometer = value;
+                }
+            }
+        }
+        
+        public double TravelledDistance
+        {
+            get { return this.travelledDistance; }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new InvalidOperationException("The travelled distance cannot be a negative number.");
+                }
+                else
+                {
+                    this.travelledDistance = value;
+                }
+            }
+        }
+
+        public bool canMakeTheTrip(double distanceToDestination, double fuelAmount, double fuelConsumption)
+        {
+            return fuelAmount - (distanceToDestination * fuelConsumption) > 0;
+        }
+
+        public void Drive(double distance, double fuelAmount, double fuelConsumption)
+        {
+            this.FuelAmount -= (distance * fuelConsumption);
+            this.TravelledDistance += distance;
+        }
+    }
+}
---
+++ CSharp_OOP_Basics/01DefinningClasses/Exercises/04_SpeedRacing/StartUp.cs
@@ -0,0 +1,58 @@
+﻿namespace DefiningClasses
+{
+    using DefiningClasses;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            int inputLines = int.Parse(Console.ReadLine());
+            Dictionary<string,Car> cars = new Dictionary<string, Car>();
+
+            for (int i = 0; i < inputLines; i++)
+            {
+                string[] carArgs = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string model = carArgs[0];
+                double fuelAmount = double.Parse(carArgs[1]);
+                double fuelConsumption = double.Parse(carArgs[2]);
+
+                Car currentCar = new Car(model, fuelAmount, fuelConsumption);
+                cars.Add(currentCar.Model, currentCar);
+            }
+
+            string command = Console.ReadLine();
+
+            while (command.ToUpper() != "END" )
+            {
+                string[] commandArgs = command.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string modelToDrive = commandArgs[1];
+                double ammountOfKm = double.Parse(commandArgs[2]);
+
+                Car carForTheTrip = cars[modelToDrive];
+                double amountOfFuel = carForTheTrip.FuelAmount;
+                double carConsumption = carForTheTrip.FuelConsumptionPerKilometer;
+
+                if (carForTheTrip.canMakeTheTrip(ammountOfKm, amountOfFuel, carConsumption))
+                {
+                    carForTheTrip.Drive(ammountOfKm, amountOfFuel, carConsumption);
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient fuel for the drive.");
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (string model in cars.Keys)
+            {
+                Car car = cars[model];
+
+                Console.WriteLine($"{car.Model} {car.FuelAmount:F2} {car.TravelledDistance}");
+            }
+        }
+    }
+}
