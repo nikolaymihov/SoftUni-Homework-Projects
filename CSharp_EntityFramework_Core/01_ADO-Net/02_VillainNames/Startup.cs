7f4e4e7f84af0f3aa1d0240c14a53a4bd8b460d6
Nikolay Mihov <Nikolay.Mihov96@gmail.com>
2020-11-26 03:02:34 +0200

    Creating a new folder "CSharp_EntityFramework_Core" and adding my first homework.
    Creating a new folder for the course "Entity Framework Core" and adding my first homework for this course.

---
+++ CSharp_EntityFramework_Core/01_ADO-Net/02_VillainNames/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Microsoft.Data.SqlClient;
+
+namespace _02_VillainNames
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            using SqlConnection dbConnection = new SqlConnection(@"Server=.;Database=Minions;Integrated Security=true;");
+            
+            dbConnection.Open();
+
+            SqlCommand command = new SqlCommand(
+                                              @"Select Name, Count(MV.MinionId) As Count from Villains AS V
+                                                Join MinionsVillains AS MV ON V.Id = MV.VillainId
+                                                Group By V.Id, V.Name
+                                                Having Count(MV.MinionId) > 3", dbConnection);
+
+            using SqlDataReader reader = command.ExecuteReader();
+
+            while(reader.Read())
+            {
+                string name = reader["Name"].ToString();
+                int count = int.Parse(reader["Count"].ToString());
+
+                Console.WriteLine("{0} - {1}", name, count);
+            }
+        }
+    }
+}
---
+++ CSharp_EntityFramework_Core/01_ADO-Net/03_MinionNames/StartUp.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+
+using Microsoft.Data.SqlClient;
+
+namespace _03_MinionNames
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int villainId = int.Parse(Console.ReadLine());
+            StringBuilder result = new StringBuilder();
+
+            using SqlConnection dbConnection = new SqlConnection(@"Server=.;Database=Minions;Integrated Security=true;");
+            
+            dbConnection.Open();
+
+            string getVillainNameQuery = @"Select [Name] From Villains Where Id = @villainId";
+
+            using SqlCommand getVillainNameCommand = new SqlCommand(getVillainNameQuery, dbConnection);
+            getVillainNameCommand.Parameters.AddWithValue("@villainId", villainId);
+
+            string villainName = getVillainNameCommand.ExecuteScalar()?.ToString();
+
+            if (villainName == null)
+            {
+                result.AppendLine($"No villain with ID {villainId} exists in the database.");
+            }
+            else
+            {
+                result.AppendLine($"Villain: {villainName}");
+
+                string getMinionsInfoQuery = @"Select m.[Name], m.Age FROM Villains AS V
+                                               LEFT JOIN MinionsVillains AS MV
+                                               ON V.Id = MV.VillainId
+                                               LEFT JOIN Minions AS M
+                                               ON mv.MinionId = M.Id
+                                               WHERE V.Id = @villainId
+                                               Order By M.[Name]";
+
+                using SqlCommand getMinionsInfoCommand = new SqlCommand(getMinionsInfoQuery, dbConnection);
+                getMinionsInfoCommand.Parameters.AddWithValue("@villainId", villainId);
+
+                using SqlDataReader reader = getMinionsInfoCommand.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    int rowNumber = 1;
+
+                    while (reader.Read())
+                    {
+                        string minionName = reader["Name"]?.ToString();
+                        string minionAge = reader["Age"]?.ToString();
+
+                        result.AppendLine($"{rowNumber}. {minionName} {minionAge}");
+
+                        rowNumber++;
+                    }
+                }
+                else
+                {
+                    result.AppendLine("(no minions)");
+                }
+            }
+
+            Console.WriteLine(result.ToString().TrimEnd());
+        }
+    }
+}
