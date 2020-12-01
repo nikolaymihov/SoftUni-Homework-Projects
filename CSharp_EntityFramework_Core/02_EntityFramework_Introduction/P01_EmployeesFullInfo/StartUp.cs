e74916fa8b18c8bf7ddc4c276b8a00fa91f934eb
Nikolay Mihov <Nikolay.Mihov96@gmail.com>
2020-12-02 01:50:32 +0200

    Adding a new folder in EF Core folder
    Adding a new folder that will contains the exercises for the "EntityFramework Introduction" lecture.

---
+++ CSharp_EntityFramework_Core/02_EntityFramework_Introduction/P01_EmployeesFullInfo/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+using P01_EmployeesFullInfo.Data;
+using P01_EmployeesFullInfo.Models;
+
+namespace P01_EmployeesFullInfo
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            string output = GetEmployeesFullInformation(context);
+
+            Console.WriteLine(output);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            List<Employee> employees = context.Employees.OrderBy(e => e.EmployeeId).ToList();
+
+            foreach (Employee employee in employees)
+            {
+                output.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
