55848bb085d2f6e6c56c8e4d95ef62b0f32af65e
Nikolay Mihov <Nikolay.Mihov96@gmail.com>
2020-12-11 00:59:42 +0200

    Adding "BookShop" database
    Adding "BooksShop" database for all the queries from the "Advanced Querying" homework from the course "C# Entity Framework Core".

---
+++ CSharp_EntityFramework_Core/05_AdvancedQuerying/BookShop.Data/BookShopContext.cs
@@ -0,0 +1,39 @@
+﻿using BookShop.Models;
+using BookShop.Data.EntityConfiguration;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShop.Data
+{
+    public class BookShopContext : DbContext
+    {
+		public BookShopContext() { }
+
+		public BookShopContext(DbContextOptions options)
+			:base(options) { }
+		
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<BookCategory> BooksCategories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AuthorConfiguration());
+            modelBuilder.ApplyConfiguration(new BookCategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new BookConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+        }
+    }
+}
---
+++ CSharp_EntityFramework_Core/05_AdvancedQuerying/BookShop/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using BookShop.Data;
+using BookShop.Initializer;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var dbContext = new BookShopContext();
+            //DbInitializer.ResetDatabase(dbContext);
+
+
+        }
+    }
+}
