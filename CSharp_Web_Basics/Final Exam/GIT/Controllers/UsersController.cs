22c0135bd24138a39b6f492e95ee0f5f02be41ae
Nikolay Mihov <Nikolay.Mihov96@gmail.com>
2021-07-18 20:08:46 +0300

    Changing the visibility of the nav-bar elements and adding Login and Register pages

---
+++ CSharp_Web_Basics/Final Exam/GIT/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿namespace GIT.Controllers
+{
+    using MyWebServer.Http;
+    using MyWebServer.Controllers;
+
+    public class UsersController : Controller
+    {
+        public HttpResponse Register() => View();
+
+        public HttpResponse Login() => View();
+    }
+}
