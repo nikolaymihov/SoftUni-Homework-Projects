cfe5409049e5abc31f4be48a6fdf2027e054a3b2
Nikolay Mihov <Nikolay.Mihov96@gmail.com>
2021-07-18 18:27:50 +0300

    Adding "GIT" project exam
    Adding "GIT" project exam with the final version of our basic web server and with the initial version of the skeleton for the exam.

---
+++ CSharp_Web_Basics/Exam/GIT/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿namespace GIT.Controllers
+{
+    using MyWebServer.Controllers;
+    using MyWebServer.Http;
+
+    public class HomeController : Controller
+    {
+        public HttpResponse Index()
+        {
+            if (this.User.IsAuthenticated)
+            {
+                return Redirect("/Cars/All");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ CSharp_Web_Basics/Exam/GIT/Startup.cs
@@ -0,0 +1,21 @@
+﻿namespace GIT
+{
+    using System.Threading.Tasks;
+    using MyWebServer;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+
+    public class Startup
+    {
+        public static async Task Main()
+            => await HttpServer
+                .WithRoutes(routes => routes
+                    .MapStaticFiles()
+                    .MapControllers())
+                .WithServices(services => services
+                    .Add<IViewEngine, CompilationViewEngine>())
+                //.WithConfiguration<CarShopDbContext>(context => context
+                  //  .Database.Migrate())
+                .Start();
+    }
+}
